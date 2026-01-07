5f4ac7cd62f521eeb460d50830a07dd60f260750
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-01-07 11:22:00 -0700

    auto-cleanup test

--- code/tests/engine/cpp/engine/caching/tile_cache_tests.cpp
+++ code/tests/engine/cpp/engine/caching/tile_cache_tests.cpp
@@ -5,11 +5,19 @@
 namespace onyx::engine::caching
 {
 
-TEST(engine_caching, tile_cache_capacity)
+class tile_cache_test : public ::testing::Test
+{
+protected:
+    void TearDown() override
+    {
+        tile_cache::shutdown();
+    }
+};
+
+TEST_F(tile_cache_test, tile_cache_capacity)
 {
     tile_cache::ref().set_capacity(2);
     ASSERT_EQ(2, tile_cache::ref().get_capacity()) << "failed to set or get capacity";
-    tile_cache::shutdown();
 }
 
 }
