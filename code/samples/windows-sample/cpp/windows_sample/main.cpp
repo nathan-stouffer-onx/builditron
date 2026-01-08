23a9e9a34b7b7a0ba65ffe510ce22512d37af4ed
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-01-08 15:57:24 -0700

    load token from assets in sample app

--- code/samples/windows-sample/cpp/windows_sample/main.cpp
+++ code/samples/windows-sample/cpp/windows_sample/main.cpp
@@ -6,10 +6,29 @@
 
 int run()
 {
+    // call add
     std::cout << "8 + 3 = " << onyx::add(8, 3) << std::endl;
+
+    // call add_layer
     std::cout << "Adding layer" << std::endl;
     onyx::add_layer(R"({ "type": "fill" })");
     std::cout << "Added layer" << std::endl;
+
+    // load token
+    std::ifstream fstream("assets/secrets/token.txt");
+    if (!fstream.is_open())
+    {
+        std::cout << "Failed to open token file" << std::endl;
+        return 1;
+    }
+    else
+    {
+        std::string line;
+        std::getline(fstream, line);
+        std::cout << "Token value: " << line << std::endl;
+        fstream.close();
+    }
+
     return 0;
 }
 
