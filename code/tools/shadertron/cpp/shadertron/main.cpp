57aa704b755c7ddecf1a68d7ea0a80bfe0263d90
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-01-09 11:28:48 -0700

    remove extraneous log

--- code/tools/shadertron/cpp/shadertron/main.cpp
+++ code/tools/shadertron/cpp/shadertron/main.cpp
@@ -10,43 +10,32 @@
 namespace onyx::shadertron
 {
 
-static constexpr char c_directory[] = "shaders";
+static constexpr std::string c_base_directory = "shaders";
 
-void log(nlohmann::json const& component)
+void touch_directory(std::string const& path)
 {
-    std::cout << "shader inputs: [ ";
-    bool first = true;
-    for (nlohmann::json const& input : component["inputs"])
+    if (!std::filesystem::exists(path))
     {
-        if (!first)
-        {
-            std::cout << ", ";
-        }
-        std::cout << input["name"];
-        first = false;
+        std::filesystem::create_directory(path);
     }
-    std::cout << " ]" << std::endl;
 }
 
 void generate(bool clean_first)
 {
     if (clean_first)
     {
-        std::filesystem::remove_all(c_directory);
+        std::filesystem::remove_all(c_base_directory);
     }
 
-    if (!std::filesystem::exists(c_directory))
-    {
-        std::filesystem::create_directory(c_directory);
-    }
+    std::filesystem::remove_all(c_base_directory + "/src");
 
-    nlohmann::json component = shaders::load();
-    log(component);
+    touch_directory(c_base_directory);
+    touch_directory(c_base_directory + "/src");
 
     std::vector<shaders::pair> pairs = shaders::generate();
     for (shaders::pair const& pair : pairs)
     {
-        std::ofstream file(std::string(c_directory) + "/" + pair.filename);
+        std::ofstream file(c_base_directory + "/src/" + pair.filename);
         file << pair.contents; 
     }
 }
@@ -55,7 +44,7 @@ int main(int argc, char* argv[])
 {
     CLI::App app("shadertron is a tool that generates shaders from families of json components.", "shadertron");
 
-    bool clean_first = true;
+    bool clean_first = false;
     app.add_option("-c,--clean-first", clean_first, "Indicates whether or not the existing output directory should be cleaned first")
         ->capture_default_str();
 
