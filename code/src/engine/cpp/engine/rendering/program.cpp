b83ada3cafaa80113070a55747581d82b4acef2b
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-01-30 16:16:23 -0700

    load programs from shaderblobs lib

--- code/src/engine/cpp/engine/rendering/program.cpp
+++ code/src/engine/cpp/engine/rendering/program.cpp
@@ -1,16 +1,17 @@
 #include "engine/rendering/program.hpp"
 
-#include <filesystem>
 #include <string>
+
+#include <shaderblobs/load.hpp>
 
 namespace onyx::engine::rendering
 {
 
 bool load_programs()
 {
-    namespace fs = std::filesystem;
-    std::string base = "assets/shaders/src/Plinth";
-    return fs::exists(base + ".vs.sc") && fs::exists(base + ".fs.sc");
+    std::string vert = shaderblobs::load("Plinth.vs.sc");
+    std::string frag = shaderblobs::load("Plinth.fs.sc");
+    return !vert.empty() && !frag.empty();
 }
 
 }
