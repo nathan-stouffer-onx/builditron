b536d4c0adf071047aee9fec33966054e95ba970
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-02-13 12:46:21 -0700

    remove host build step while configuring

--- code/cmake/ConfigureHostTools.cmake
+++ code/cmake/ConfigureHostTools.cmake
@@ -15,18 +15,6 @@ if(NOT host_configure_result EQUAL 0)
     message(FATAL_ERROR "Host configure failed")
 endif()
 
-message(STATUS "Building host")
-
-execute_process(
-  COMMAND ${CMAKE_COMMAND} --build --preset ${HOST_PRESET} --target shadertron
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  RESULT_VARIABLE host_build_result
-)
-
-if(NOT host_build_result EQUAL 0)
-    message(FATAL_ERROR "Host build failed")
-endif()
-
 # Export the path to host-built tools for use by target build
 set(HOST_SHADERTRON_EXECUTABLE "${HOST_BINARY_DIR}/code/tools/shadertron/shadertron${CMAKE_EXECUTABLE_SUFFIX}" CACHE FILEPATH "Path to host-built shadertron executable")
 message(STATUS "Host shadertron available at: ${HOST_SHADERTRON_EXECUTABLE}")
