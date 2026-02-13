b0ea89460c4783c7bf65b98a456937f34288018f
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-02-13 11:33:10 -0700

    build host tool as part of target configure/build

--- code/cmake/BuildHostTools.cmake
+++ code/cmake/BuildHostTools.cmake
@@ -3,6 +3,7 @@
 message(STATUS "Configuring host")
 
 set(HOST_PRESET "host-win-Debug")
+set(HOST_BINARY_DIR "${CMAKE_SOURCE_DIR}/out/build/${HOST_PRESET}")
 
 execute_process(
   COMMAND ${CMAKE_COMMAND} --preset ${HOST_PRESET}
@@ -25,3 +26,7 @@ execute_process(
 if(NOT host_build_result EQUAL 0)
     message(FATAL_ERROR "Host build failed")
 endif()
+
+# Export the path to host-built tools for use by target build
+set(HOST_SHADERTRON_EXECUTABLE "${HOST_BINARY_DIR}/code/tools/shadertron/shadertron${CMAKE_EXECUTABLE_SUFFIX}" CACHE FILEPATH "Path to host-built shadertron executable")
+message(STATUS "Host shadertron available at: ${HOST_SHADERTRON_EXECUTABLE}")
