d57883dda878da7ecb9e8e1d013421fe6d9bef77
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-03-16 15:01:31 -0600

    formatting

--- code/src/mapitron/cpp/bindings/embind.cpp
+++ code/src/mapitron/cpp/bindings/embind.cpp
@@ -3,7 +3,8 @@
 
 using namespace emscripten;
 
-EMSCRIPTEN_BINDINGS(mapitron) {
+EMSCRIPTEN_BINDINGS(mapitron)
+{
     // Expose the mapitron API to JavaScript
     function("shutdown", &mapitron::shutdown);
     function("add", &mapitron::add);
