d0773c8b8fa8c5ed3cce6796a98468b4f80bc408
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2026-04-03 16:08:32 -0600

    configure cmake projects when generating iOS projects

--- code/samples/direct/ios-sample/bin/generate_projects.sh
+++ code/samples/direct/ios-sample/bin/generate_projects.sh
@@ -4,11 +4,11 @@ set -e
 REPO_ROOT="$(cd "$(dirname "$0")/../../../../.." && pwd)"
 SAMPLES_DIR="$REPO_ROOT/code/samples/direct/ios-sample"
 
-# echo "Configuring CMake for simulator..."
-# cmake --preset dest-ios-simulator-xcode -S "$REPO_ROOT"
+echo "Configuring CMake for simulator..."
+cmake --preset dest-ios-simulator-xcode -S "$REPO_ROOT"
 
-# echo "Configuring CMake for device..."
-# cmake --preset dest-ios-device-xcode -S "$REPO_ROOT"
+echo "Configuring CMake for device..."
+cmake --preset dest-ios-device-xcode -S "$REPO_ROOT"
 
 echo "Generating Xcode workspace..."
 (cd "$SAMPLES_DIR" && tuist generate)
